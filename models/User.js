1c746ba92bec7c392006c019687fbe0e1adc9414
nickNyamu <nicholas.nyamu@strathmore.edu>
2024-06-17 14:11:08 +0300

    refactor: Add User model for storing user information

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+// Load User model
+const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
