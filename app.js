7744e69b3915c51468e69874075620aa2e5afb01
nickNyamu <nicholas.nyamu@strathmore.edu>
2024-06-17 16:31:44 +0300

    feat: Update app.js with body parser and handlebars helpers

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const {engine} = require("express-handlebars");
+const { engine } = require("express-handlebars");
 const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo"); //Pass session to connect-mongo to store session in the database
@@ -19,24 +19,39 @@ connectDB();
 
 const app = express();
 
-//Express session middleware
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
-}));
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+//Express session middleware
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
 
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Handlebars Helpers
+const { formatDate, truncate, stripTags, editIcon, select } = require("./helpers/hbs");
+
 // Configure Handlebars view engine
 app.engine(
   ".hbs",
   engine({
+    helpers: {
+      formatDate, // Register the formatDate helper
+      truncate,
+      stripTags,
+      editIcon,
+      select,
+    },
     defaultLayout: "main",
     extname: ".hbs",
   })
@@ -47,13 +62,19 @@ app.set("view engine", ".hbs");
 app.use(passport.initialize()); //Initialize passport
 app.use(passport.session()); //Use passport session having installed express-session
 
+//  Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null; // Set the user to the logged in user
+  next();
+});
+
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
-
 
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 5000;
 
