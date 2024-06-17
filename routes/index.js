85f2dc095a437d67c10881eef810008bbf0b9eaf
nickNyamu <nicholas.nyamu@strathmore.edu>
2024-06-17 14:28:30 +0300

    feat: Update dashboard view with user's first name in welcome message

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,11 @@ router.get("/", ensureGuest, (req, res) => {
 // @desc    Dashboard
 // @route   GET /dashboard
 router.get("/dashboard", ensureAuth, (req, res) => {
-  console.log(req.user);
-  res.render("dashboard"); // Render the dashboard view
+  // console.log(req.user); // { _id: 5f3f5c8f7e5f3b1f9c8e3b6d, googleId: '106693246588098883000', displayName: 'John Doe', __v: 0}
+  res.render("dashboard", {
+    name: req.user.firstName,
+  
+  }); // Render the dashboard view
 });
 
 module.exports = router;
